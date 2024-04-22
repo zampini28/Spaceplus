bd6820f6056e24c1a51d7c92af2837e77cc0f347
zampini28 <caio.zampini@gmail.com>
2024-04-22 09:04:51 -0300

    more film

--- webapps/spaceplus/sql/load.sql
+++ webapps/spaceplus/sql/load.sql
@@ -16,3 +16,58 @@ VALUES
  ('Oppenheimer', 4.6, '20-07-2023', 'Christopher Nolan', 'Em 1926, o estudante de doutorado J. Robert Oppenheimer, de 22 anos, estuda com o físico experimental Patrick Blackett no Laboratório Cavendish, em Cambridge. Após muito tempo longe de seu lar e sofrendo de ansiedade em decorrência do trabalho no laboratório, Oppenheimer deixa para o exigente Blackett uma maçã envenenada, porém, a recupera das mãos do cientista visitante Niels Bohr, que fica impressionado o suficiente com seu intelecto para recomendar que ele estude física teórica na Alemanha, onde Oppenheimer completa seus estudos do PhD. Mais tarde, conhece o físico teórico Werner Heisenberg em uma conferência na Suíça.', '3h 00m', 96.99, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5b/Oppenheimer_poster.jpg/250px-Oppenheimer_poster.jpg'),
  ('Taxi Driver', 4.1, '08-02-1976', 'Martin Scorsese', 'Travis Bickle é um jovem de 26 anos frustrado e alienado do meio-oeste dos Estados Unidos, que alega ter sido recentemente dispensado do Corpo de Fuzileiros Navais. Ele sofre de insônia e consequentemente arranja um emprego como taxista na cidade de Nova Iorque, oferecendo-se para trabalhar no turno da madrugada. Travis passa o seu tempo livre assistindo a filmes pornográficos em cinemas imundos, dirigindo sem rumo pela periferia de Manhattan. Também observa Nova York de seu táxi e irrompe com violência contra o que julga ser a escória que contamina a cidade.', '1h 00m', 60.87, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/3/33/Taxi_Driver_%281976_film_poster%29.jpg');
 
+
+ ('Os Bons Companheiros', 4.6, '19-11-1990', 'Martin Scorsese', 'Contada em primeira pessoa pelo ítalo-irlandês Henry Hill (Ray Liotta), o filme traça uma biografia da Máfia. Ainda jovem, Henry se envolve com Tommy DeVito (Joe Pesci) e James Conway (Robert De Niro) e se casa com Karen (Lorraine Bracco), jovem judia que vê toda a sua vida social se misturando com o crime.', '2h 45m', 76.15, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/7/7c/GoodfellasPoster.jpg/250px-GoodfellasPoster.jpg');
+
+
+
+ ('O Irlandês', 4.9, '27-11-2019', 'Martin Scorsese', 'Em setembro de 2014, Scorsese confirmou que o seu próximo projeto, após terminar o filme Silence de 2016, seria o de realizar O Irlandês. De Niro e Pacino foram confirmados naquele mês, assim como Pesci, que depois de ser convidado várias vezes para assumir o papel, regressou à representação, interrompendo a sua não oficial aposentadoria. A fotografia principal começou em setembro de 2017 na cidade de Nova Iorque, nos bairros de Mineola e Williston Park de Long Island, terminando apenas em março de 2018. Com um orçamento de produção de 159 milhões de dólares, é um dos filmes mais caros da carreira de Scorsese.', '3h 25m', 99.99, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/d/da/The_Irishman_p%C3%B4ster.png/235px-The_Irishman_p%C3%B4ster.png');
+
+
+
+ ('Amnésia', 3.6, '16-03-2001', 'Christopher Nolan', 'Memento é apresentado em duas sequências diferentes de cenas. Um série em preto e branco que é mostrada cronologicamente e uma série de cenas coloridas que são mostradas em ordem reversa. As duas sequências se encontram no final do filme, produzindo uma única história em comum. Homem cuja mulher foi barbaramente assassinada vai atrás do assassino, tendo como obstáculo sua amnésia.', '1h 54m', 39.99, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/c/cf/MementoCartaz.jpg/250px-MementoCartaz.jpg');
+
+
+ ('A Origem', 4.1, '08-07-2010', 'Christopher Nolan', 'Dom Cobb, juntamente com seu organizador e braço direito Arthur, estão em uma missão de invasão da mente do poderoso empresário japonês Saito usando uma técnica de espionagem industrial através dos sonhos. A dor é sentida nos sonhos, porém a morte leva a pessoa a acordar. Cobb carrega consigo um totem na forma de pião, que originalmente pertencia à sua falecida esposa Mal, para determinar se ele está sonhando ou não. A extração falha devido a intervenção de Mal, cujas lembranças assombram a mente de Cobb e sabotam suas missões. Saito revela que ele estava, na verdade, fazendo um teste para saber se a equipe tinha capacidade de fazer uma inserção, ou seja, usar os sonhos de uma pessoa para implantar a origem de uma ideia. Em troca, ele promete livrar todas as acusações de assassinato contra Cobb, se a missão for bem sucedida, para que ele possa rever seus filhos nos Estados Unidos.', '1h 48m', 71.67, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/8/84/AOrigemPoster.jpg/230px-AOrigemPoster.jpg');
+
+
+ ('O Terceiro Passo', 3.5, '20-09-2006', 'Christopher Nolan', 'A história segue Robert Angier e Alfred Borden, mágicos rivais de Londres no final do século 19. Obcecados em criar o melhor truque de ilusão, eles iniciam uma competição com trágicos resultados.', '1h 10m', 25.79, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/8/8c/The_Prestige.jpg/250px-The_Prestige.jpg');
+
+ ('De Volta para o Futuro', 4.8, '20-12-1985', 'Robert Zemeckis', 'Uma máquina do tempo construída por um cientista é acionada acidentalmente por um jovem, de modo a voltar à década de 1950. Antes do casamento dos pais, o cientista conhece sua mãe, que está apaixonada pelo pai dele. Essa paixão coloca a própria existência em risco, porque todo o futuro poderia ser alterado de modo a forçar o amor entre os pais.', '1h 16m', 54.89, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/97/BackFuturePoster.jpg/240px-BackFuturePoster.jpg');
+
+ ('Eu Sou a Lenda', 4.7, '14-12-2007', 'Francis Lawrence', 'Em 2009, o vírus do sarampo foi geneticamente alterado, originalmente para ser uma cura para o câncer, porém acaba se transformando em uma cepa letal que mata 90% dos infectados, transforma 9% em vampiros que se alimentam de sangue e são extremamente vulneráveis à luz solar (Por isso o nome). Três anos depois, o cientista Robert Neville (Will Smith) é o único sobrevivente de Nova Iorque (marco zero da disseminação do vírus) e acredita estar sozinho na cidade, sendo o único sobrevivente. Por algum motivo que nem ele conhece, é imune ao vírus, que se propaga pelo ar e por mordidas.', '1h 30m', 80.59, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/d/df/I_am_legend_teaser.jpg/230px-I_am_legend_teaser.jpg');
+
+ ('Um Sonhe de Liberdade', 4.3, '14-09-1994', 'Frank Darabont', 'Em Portland (Maine), 1947, o banqueiro Andy Dufresne é condenado pelo assassinato de sua esposa e do amante dela, sendo sentenciado a duas prisões perpétuas a serem cumpridas na Penitenciária Estadual de Shawshank. Lá, ele fica amigo do contrabandista Ellis Boyd Red Redding, outro detento que também está servindo em prisão perpétua. Red adquire um pequeno martelo de geólogo e depois um pôster de Rita Hayworth a pedido de Andy. Este acaba sendo regularmente assediado e estuprado enquanto trabalha na lavanderia da prisão por uma gangue de detentos chamada de As Irmãs e seu líder Bogs Diamond.', '1h 30m', 37.91, 'Inglês', 'https://upload.wikimedia.org/wikipedia/pt/thumb/d/d2/The_Shawshank_Redemption_p%C3%B4ster.png/240px-The_Shawshank_Redemption_p%C3%B4ster.png');
+
+
+um sonho de liberdade
+
+21 gramas
+
+birdman
+
+amores brutos
+
+os oito odiados
+
+a chegada
+
+gangues de nova york
+
+prenda-me se for capaz
+
+o terminal
+
+crimes e pecados
+
+the kid
+
+laranja mecânica
+
+glória feita de sangue incêndios
+
+vítimas da tormenta
+
+django livre
+
+manhattan
